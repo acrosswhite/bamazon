cfb60fd8d2bacdea332a266c276540ea2f3d6d91
Ashley Crosswhite <ar.crosswhite@gmail.com>
2017-10-28 16:09:37 -0700

    update js

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,3 +1,7 @@
+
+var mysql = require("mysql");
+var inquirer = require("inquirer")
+
 var connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
@@ -16,3 +20,36 @@ connection.connect(function(err) {
   displayProducts();
 });
 
+function displayProducts(){
+  var productQueryAll = "SELECT id, product_name, price FROM products";
+  
+  connection.query(productQueryAll, function(err, data){
+    if (err) throw err;
+    for (var i = 0; i < data.length; i++) {
+      var row = data[i]
+      console.log("Product Code " + row.id + "\nProduct: " + row.product_name + "\nPrice: " + row.price + "\n");
+    }
+    inquirer.prompt({
+      name: "productId",
+      message: "What is the ID of the product you would like?"
+    },
+    {
+      name: "units",
+      message: "How many would you like?"
+    }).then(function(answers){
+      for (var i = 0; i < data.length; i++) {
+        if (answers.productId === row.id){
+          //check for quantity
+          console.log("Thank you for your order")
+          //update database with new totals
+          console.log("Customer Total: " )
+          //give customer a total cost of quantity and price
+        }
+
+      }
+      //if no product left
+      console.log("Insufficient quantity")
+    })
+          connection.end();
+  });
+}
