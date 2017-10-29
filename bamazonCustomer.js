e0daf907d2f612a5d3ec3d5d3b91a97b7297b192
Ashley Crosswhite <ar.crosswhite@gmail.com>
2017-10-29 11:04:11 -0700

    update customer.js

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -20,8 +20,10 @@ connection.connect(function(err) {
   displayProducts();
 });
 
+
+
 function displayProducts(){
-  var productQueryAll = "SELECT id, product_name, price FROM products";
+  var productQueryAll = "SELECT id, product_name, price, stock_quantity FROM products";
   
   connection.query(productQueryAll, function(err, data){
     if (err) throw err;
@@ -29,27 +31,64 @@ function displayProducts(){
       var row = data[i]
       console.log("Product Code " + row.id + "\nProduct: " + row.product_name + "\nPrice: " + row.price + "\n");
     }
-    inquirer.prompt({
+    inquirer.prompt([
+    {
       name: "productId",
       message: "What is the ID of the product you would like?"
     },
     {
       name: "units",
       message: "How many would you like?"
-    }).then(function(answers){
-      for (var i = 0; i < data.length; i++) {
-        if (answers.productId === row.id){
-          //check for quantity
-          console.log("Thank you for your order")
-          //update database with new totals
-          console.log("Customer Total: " )
-          //give customer a total cost of quantity and price
-        }
 
+    }
+    ]).then(function(answers){
+      console.log(answers)
+      for (var j = 0; j < data.length; j++) {
+        var productRow = data[j]
+        if (answers.productId == productRow.id){
+         // console.log(productRow)
+         //check for quantity
+         //console.log(parseInt(answers.units))
+         //console.log(typeof(productRow.stock_quantity))
+          if(parseInt(answers.units) <= productRow.stock_quantity){
+
+            console.log("Thank you for your order")
+            //update database with new totals
+            console.log("\nCustomer Total: " + (parseInt(answers.units))*(productRow.price))
+            console.log("_______________________________")
+            updateQuantity(answers.units, answers.productId);
+
+          }
+          //if no product left
+          else if (parseInt(answers.units) > productRow.stock_quantity){
+          console.log("Insufficient quantity")
+        }
       }
-      //if no product left
-      console.log("Insufficient quantity")
-    })
-          connection.end();
+    }
+
+  })
+//          connection.end();
   });
 }
+
+
+function updateQuantity(units, productId){
+
+var productUpdate = connection.query(
+  "UPDATE products SET ? WHERE ?",
+  [{
+    stock_quantity: units
+  },
+  {
+    id: productId
+  }],
+  function(err, result){
+    console.log(result)
+    if (err) throw err;
+    console.log(result.affectedRows + " quantity updated\n")
+  });
+}
+
+
+
+
